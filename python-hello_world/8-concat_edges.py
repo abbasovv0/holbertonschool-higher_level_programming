67589d4d7c44ac2ccf262281d93a7c2e2c0b3e56
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-14 11:54:33 +0000

    concat_edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,5 +2,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 words = str.split()
-new_sent = words[5] +  " " + words[6] + " " + words[12] + " " + words[0]
+new_sent = f"{words[5]} {words[6]} {words[12]} {words[0]}"
 print(new_sent)
