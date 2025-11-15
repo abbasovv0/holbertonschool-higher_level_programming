4d1c1f26a333fa5d50cb639dbe909c5c9ed0bc28
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-15 08:05:23 +0000

    piton

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
 
@@ -5,5 +6,5 @@ if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
-else:
+elif: number < 0
     print(f"{number} is negative")
