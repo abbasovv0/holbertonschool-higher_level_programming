45a080a19178cc6c2cc8acd305fea6653329dc53
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-21 08:27:28 +0000

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    new = f(tuple_a)
+    new1 = f(tuple_b)
+    return (new[0] + new1[0], new[1] + new1[1])
     
-    a0 = tuple_a[0] if len(tuple_a) > 0 else 0
-    a1 = tuple_a[1] if len(tuple_a) > 1 else 0
-    b0 = tuple_b[0] if len(tuple_b) > 0 else 0
-    b1 = tuple_b[1] if len(tuple_b) > 1 else 0
-    return (a0 + b0, a1 + b1)
-if __name__ == "__main__":
-    test_cases = [
-        ((1, 2), (1, 2)),
-        ((1, 2), (1, 2, 3)),
-        ((1, 2, 3), (1, 2)),
-        ((1, 2, 3), (1, 2, 3)),
-        ((1,), (1, 2)),
-        ((), (1, 2)),
-        ((1, 2), (1,)),
-        ((1, 2), ()),
-        ((1,), (1,)),
-        ((), ()),
-    ]
-    for t_a, t_b in test_cases:
-        print(f"{t_a} + {t_b} = {add_tuple(t_a, t_b)}")
-
+def f(t=()):
+    if len(t) == 2:
+        return(t[0],t[1])
+    elif len(t) < 2:
+     return( t[0],0)
+    elif len(t) > 2:
+        return (t[0],t[1])
