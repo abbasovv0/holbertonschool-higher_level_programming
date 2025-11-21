a4c65deec19c392aaf4af30ef8c65809d5aed352
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-21 07:44:23 +0000

    max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -9,5 +9,3 @@ def max_integer(my_list=[]):
         if num > largest_num:
             largest_num = num
     return largest_num
-my_list = [3,4,1,0,-2,7,9,10]
-print(f"biggest number:{max_integer(my_list)}")
