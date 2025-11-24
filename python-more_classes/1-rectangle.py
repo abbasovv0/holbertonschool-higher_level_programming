266a18629b081d751e8fdf4bb569fefc9f229663
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-24 18:19:42 +0000

    44

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-"""
-This module defines a Rectangle class with width and height.
-"""
+"""this is document"""
+
+
 class Rectangle:
-    """A class representing a rectangle with width and height."""
+    """this is document for"""
+
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle instance."""
+        """this is document for"""
         self.width = width
         self.height = height
+
     @property
     def width(self):
-        """Get the width of the rectangle."""
+        """this is document for """
         return self.__width
+
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle with validation."""
+        """if is not documnet"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
-        """Get the height of the rectangle."""
+        """return value"""
         return self.__height
+
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle with validation."""
+        """return height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
