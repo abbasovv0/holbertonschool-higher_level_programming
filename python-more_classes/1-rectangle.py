a9448e1c49eb86261afd4fe6e6a7019afd1cb9fd
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-24 18:14:55 +0000

    44

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -2,21 +2,16 @@
 """
 This module defines a Rectangle class with width and height.
 """
-
-
 class Rectangle:
     """A class representing a rectangle with width and height."""
-
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle instance."""
         self.width = width
         self.height = height
-
     @property
     def width(self):
         """Get the width of the rectangle."""
         return self.__width
-
     @width.setter
     def width(self, value):
         """Set the width of the rectangle with validation."""
@@ -25,12 +20,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     @property
     def height(self):
         """Get the height of the rectangle."""
         return self.__height
-
     @height.setter
     def height(self, value):
         """Set the height of the rectangle with validation."""
@@ -39,4 +32,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
