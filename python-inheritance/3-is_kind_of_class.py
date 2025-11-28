d9e8754be2f312bc1fb6186cce1b44e15835e6b0
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-28 07:16:26 +0000

    kecmisiyde_ise

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """this is documented"""
+
+
 def is_kind_of_class(obj, a_class):
-	"""this is documented"""
-	return isinstance(obj, a_class)
+    """this is documented"""
+    return isinstance(obj, a_class)
