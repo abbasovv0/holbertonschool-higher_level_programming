5f21d9623f8d8e1b4c759bfaf30051a0ed774a85
abbasovv0 <abbasabbasov569@gmail.com>
2025-11-28 08:39:01 +0000

    geoo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""this module documented"""
-
+"""This module defines the BaseGeometry class."""
 
 
 class BaseGeometry:
-	"""CLASS IS DOCUMENTED"""
-	def area(self):
-		raise Exception("area() is not implemented")
-	def integer_validator(self, name, value):
-		if type(value) is not int:
-    			raise TypeError(f"{name} must be an integer")
-		if value <= 0:
-   			raise ValueError(f"{name} must be greater than 0")
+    """BaseGeometry class documentation."""
 
+    def area(self):
+        """Public instance method that raises an exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that 'value' is a positive integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
