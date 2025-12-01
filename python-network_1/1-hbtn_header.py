babc754044eb18b48c44f69390f21c21ec216535
abbasovv0 <abbasabbasov569@gmail.com>
2025-12-01 06:38:44 +0000

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,7 @@
 import sys
 import urllib.request
 
-url = sys.argv[https://intranet.hbtn.io]
+url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
     print(response.headers.get("X-Request-Id"))
