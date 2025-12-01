3684074306fb195d936be0d0a8ab50921f24a3b6
abbasovv0 <abbasabbasov569@gmail.com>
2025-12-01 06:40:13 +0000

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""Fetches a URL and displays the X-Request-Id header value."""
+
 import sys
 import urllib.request
 
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get("X-Request-Id"))
+def main():
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get("X-Request-Id")
+        if x_request_id:
+            print(x_request_id)
+
+
+if __name__ == "__main__":
+    main()
+
