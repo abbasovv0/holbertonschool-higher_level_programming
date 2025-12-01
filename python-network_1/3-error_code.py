88c12f256072c04444637b05528b309162bf744f
abbasovv0 <abbasabbasov569@gmail.com>
2025-12-01 07:00:48 +0000

    error_code

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""DOCUMENTED."""
+
+import sys
+import urllib.request
+import urllib.error
+
+
+def main():
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    main()
